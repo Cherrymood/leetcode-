5a1a3e57d794e76d433912aae4695eebfa40054d
Valeriia <olxlerasport@gmail.com>
2023-04-11 08:51:09 -0700

    Added solution1342

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{ 
+    static void Main(string[] args)
+    { 
+        var solution1342 =  new Solution();
+        var answer = solution1342.NumberOfSteps(14);
+        var answer1 = solution1342.NumberOfSteps(8);
+        var answer2 = solution1342.NumberOfSteps(4);
+        Console.WriteLine(answer);
+        Console.WriteLine(answer1);
+        Console.WriteLine(answer2);
+
+    }
+}
---
+++ Solution1342.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int NumberOfSteps(int num) {
+
+        int answer = 0;              
+
+        while (num !=0)
+        {
+            if (num % 2 == 0)
+            {
+                num /= 2;
+                answer += 1;
+            }
+            else
+            {
+                num -=1;
+                answer +=1;
+            }
+        }
+        return answer;
+ 
+    }
+}
