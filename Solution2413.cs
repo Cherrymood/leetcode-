302b93704a1868317e0f6efc28da825a3e026a67
Valeriia <olxlerasport@gmail.com>
2023-04-11 09:02:19 -0700

    Added solution2413

---
+++ Solution2413.cs
@@ -0,0 +1,12 @@
+    public class Solution2413 {
+        public int SmallestEvenMultiple(int n) {
+            if (n%2==0)
+            {
+                return n;
+            }
+            else
+            {
+                return n*2;
+            }
+        }
+    }
