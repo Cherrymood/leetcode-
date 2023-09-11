3b23eda4db85a051471e0d8e22452f7c22121c13
Valeriia <olxlerasport@gmail.com>
2023-09-11 12:17:21 -0700

    Added solution872

---
+++ Solution872.cs
@@ -0,0 +1,26 @@
+public class Solution872 {
+    
+    public bool LeafSimilar(TreeNode root1, TreeNode root2) {
+
+    var leaves1 = new List<int>();
+    var leaves2 = new List<int>();
+    dfs(root1, leaves1);
+    dfs(root2, leaves2);
+    return leaves1.SequenceEqual(leaves2);
+    }
+        
+    public void dfs(TreeNode node, List<int> leafValues){
+        
+        if ( node == null)
+        { 
+            return;
+        }
+
+        if (node.left == null && node.right == null)
+            {
+                leafValues.Add(node.val);
+            }
+        dfs(node.left, leafValues);
+        dfs(node.right, leafValues);
+    }
+}
