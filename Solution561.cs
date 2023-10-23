9ba555e5011b570b73eb7dfb5f0c80df62a7a5b8
Valeriia <valeriia@Sergeys-MacBook-Pro.local>
2023-10-23 16:59:34 -0700

    Edited my Solution561

--- Solution561.cs
+++ Solution561.cs
@@ -5,12 +5,9 @@ public class Solution561 {
         Array.Sort(nums);
 
 
-        for (int i = 0; i < nums.Length; i++)
+        for (int i = 0; i < nums.Length - 1; i+=2)
         {
-            for (int k = i+1; k < nums.Length; k++)
-            {
-                answer += Math.Min(nums[i], nums[k]);
-            }
+            answer += nums[i];
         }
 
         return answer;
