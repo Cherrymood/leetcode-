0ec4a98d96ab219fe4b27b4b7e1cb962ab52a94e
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-11-29 16:32:56 -0800

    Update Solution1302.cs

--- Solution1302.cs
+++ Solution1302.cs
@@ -1,55 +1,41 @@
-public class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node(int val) {
-        this.val = val;
-        this.left = left;
-        this.right = right;     }
-}
 public class Solution {
-
+    
     int sum = 0;
-    int maxDept = 0;
+    int depth = 0;
 
-    public int FindDepth(Node current, int level)
-    {
-        int maxDept = level;
-
-        if (current.left != null)
-        {
-            maxDept = FindDepth(current.left, level +1);
-        }
-
-        if (current.right != null)
-        {
-            maxDept = Math.Max( maxDept, FindDepth(current.right, level +1));
-        }
-
-        return maxDept;
-
-    }
-
-    public void FindLeaf(Node root, int level)
+    public int findDepth(TreeNode root)
     {
         if (root == null)
+        {
+            return 0;
+        }
+
+        return Math.Max(findDepth(root.left), findDepth(root.right)) + 1;
+    }
+
+    public void findLeaf(TreeNode root, int level)
+    {
+        if(root == null)
         {
             return;
         }
 
-        if (root.right == null && root.left == null && level == maxDept)
+        if(root.left == null && root.right == null && level == depth)
         {
             sum += root.val;
         }
-
-        FindLeaf(root.left, level +1);
-        FindLeaf(root.right, level +1);
-
+        else
+        {
+            findLeaf(root.left, level+1);
+            findLeaf(root.right, level+1);
+        }
+        
     }
-    public int DeepestLeavesSum(Node root) 
-    {
-        maxDept = FindDepth(root, 0);
-        FindLeaf(root, 0);
+
+    public int DeepestLeavesSum(TreeNode root) {
+        
+        depth = findDepth(root);
+        findLeaf(root, 1);
 
         return sum;
     }
