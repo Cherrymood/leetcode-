b371ed685e371ad8d998c70a7938d17146fe7c33
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-12-13 10:37:24 -0800

    Create Solution100.cs

---
+++ Solution100.cs
@@ -0,0 +1,10 @@
+public class Solution100 {
+ 
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        if(p == q) return true;
+        if(p == null || q == null || p.val != q.val) return false;
+
+        return  IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+
+    }
+}
